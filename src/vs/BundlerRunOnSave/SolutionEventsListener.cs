5c6eaae6d6070eb29483925474f2b6510887b682
Nick Larsen <fodylarsen@gmail.com>
2012-06-11 16:17:21 -0400

    Add visual studio extension to run bundler when saving files

---
+++ src/vs/BundlerRunOnSave/SolutionEventsListener.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace ServiceStack.BundlerRunOnSave
+{
+    // this class only supports the OnAfterOpenSolution event right now, 
+    // but the pattern is easy to update if you need more events
+    internal class SolutionEventsListener : IVsSolutionEvents, IDisposable
+    {
+        private IVsSolution solution;
+        private uint solutionEventsCookie;
+
+        public event Action OnAfterOpenSolution;
+
+
+        public SolutionEventsListener()
+        {
+            InitNullEvents();
+
+            solution = Package.GetGlobalService(typeof(SVsSolution)) as IVsSolution;
+
+            if (solution != null)
+            {
+                solution.AdviseSolutionEvents(this, out solutionEventsCookie);
+            }
+        }
+
+        private void InitNullEvents()
+        {
+            OnAfterOpenSolution += () => { };
+        }
+
+        #region IVsSolutionEvents Members
+
+        int IVsSolutionEvents.OnAfterCloseSolution(object pUnkReserved)
+        {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnAfterLoadProject(IVsHierarchy pStubHierarchy, IVsHierarchy pRealHierarchy)
+        {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnAfterOpenProject(IVsHierarchy pHierarchy, int fAdded)
+        {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnAfterOpenSolution(object pUnkReserved, int fNewSolution)
+        {
+            OnAfterOpenSolution();
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnBeforeCloseProject(IVsHierarchy pHierarchy, int fRemoved)
+        {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnBeforeCloseSolution(object pUnkReserved)
+        {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnBeforeUnloadProject(IVsHierarchy pRealHierarchy, IVsHierarchy pStubHierarchy)
+        {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnQueryCloseProject(IVsHierarchy pHierarchy, int fRemoving, ref int pfCancel)
+        {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnQueryCloseSolution(object pUnkReserved, ref int pfCancel)
+        {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnQueryUnloadProject(IVsHierarchy pRealHierarchy, ref int pfCancel)
+        {
+            return VSConstants.S_OK;
+        }
+
+        #endregion
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            if (solution != null && solutionEventsCookie != 0)
+            {
+                GC.SuppressFinalize(this);
+                solution.UnadviseSolutionEvents(solutionEventsCookie);
+                OnAfterOpenSolution = null;
+                solutionEventsCookie = 0;
+                solution = null;
+            }
+        }
+
+        #endregion
+    }
+}
