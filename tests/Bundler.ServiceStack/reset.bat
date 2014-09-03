92a4236cd6919021ab6456fa02d8ccf8a6e5c4d3
Demis Bellot <demis.bellot@gmail.com>
2014-09-03 19:35:26 -0400

    Add updated modules

---
+++ tests/Bundler.ServiceStack/reset.bat
@@ -0,0 +1,30 @@
+DEL Content\test.css
+DEL Content\test.min.css
+DEL Content\test.js
+DEL Content\test.min.js
+
+DEL Content\css\test-css.min.css
+DEL Content\css\test-less.css
+DEL Content\css\test-less.min.css
+DEL Content\css\test-sass.css
+DEL Content\css\test-sass.min.css
+DEL Content\css\test-scss.css
+DEL Content\css\test-scss.min.css
+DEL Content\css\test-stylus.css
+DEL Content\css\test-stylus.min.css
+
+DEL Content\js\test-coffeescript.js
+DEL Content\js\test-coffeescript.min.js
+DEL Content\js\test-javascript.min.js
+DEL Content\js\test-livescript.js
+DEL Content\js\test-livescript.min.js
+
+
+
+
+
+
+
+
+
+
